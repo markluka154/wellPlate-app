61ec7bf1018a7dcee5363f5a338eed71143b0c73
Medja <medja.mark@gmail.com>
2025-10-18 12:10:56 +0200

    Implement exact Facebook Pixel code with @ts-ignore

--- apps/web/src/components/FacebookPixel.tsx
+++ apps/web/src/components/FacebookPixel.tsx
@@ -10,27 +10,20 @@ declare global {
 
 export function FacebookPixel() {
   useEffect(() => {
-    // Meta Pixel Code - Official Facebook implementation (TypeScript compatible)
-    (function(f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
-      if (f.fbq) return
-      n = f.fbq = function() {
-        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-      }
-      if (!f._fbq) f._fbq = n
-      n.push = n
-      n.loaded = !0
-      n.version = '2.0'
-      n.queue = []
-      t = b.createElement(e)
-      t.async = !0
-      t.src = v
-      s = b.getElementsByTagName(e)[0]
-      s.parentNode.insertBefore(t, s)
-    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
-
+    // Meta Pixel Code - Exact Facebook implementation
+    // @ts-ignore - Facebook's official code
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    
     // Initialize Facebook Pixel
-    window.fbq('init', '788765987308138')
-    window.fbq('track', 'PageView')
+    window.fbq('init', '788765987308138');
+    window.fbq('track', 'PageView');
   }, [])
 
   return (
